77e4b99d025cc317c0f3e66a8282d9cdd02a7824
MaximusFT <maximusft@gmail.com>
2016-04-25 23:55:52 +0300

    update module lastes link

---
+++ core/modules/mod-link-latest/link-latest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     sCMS.Module
+ * @subpackage  mod_link-favorite
+ *
+ * @copyright   Copyright (C) 2005-2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * $res - global variable
+ * $db - global variable to use MySQL
+ * $rd - global variable to use TimeFormat
+ * $modRes - main variable with all params by Module
+ */
+
+defined('ISsCMS') or die;
+
+$modPopularArticle = $db->select("content", [
+        "id",
+        "alias",
+        "h1",
+        "hits"
+    ], [
+        "lang" => USER_LANG,
+        "ORDER" => array('publish_up DESC'),
+        "LIMIT" => 5
+    ]);
+
+include $modPathView;
+?>
