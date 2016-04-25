110955c068c6ccacc005578e065ef54a9b049281
MaximusFT <maximusft@gmail.com>
2016-04-25 23:55:26 +0300

    fix menu active element form alias to ID

--- core/modules/mod-menu/menu-category.php
+++ core/modules/mod-menu/menu-category.php
@@ -20,49 +20,15 @@ defined('ISsCMS') or die;
  * @param 	String 	$active Alias menu item that is Active
  * @return 	HTML 			Return builded HTML
  */
-function frontMenuBuildCategory($params, $res, $active = null) {
-    $html = '';
-    foreach($params as $key => $value) {
-        $i == 0;
-        foreach($value as $key => $index) {
-            $i++;
-            if($key == 'id') $tempId = $index;
-            if ($res[$value['id']]['id'] == $value['id']) {
-                $url = menuLinkBuilder('category', $res[$value['id']]['id']);
-                $id = $res[$value['id']]['id'];
-                $title = $res[$value['id']]['title'];
-                $alias = ($active == $res[$value['id']]['alias'])?' active':'';
-                if(is_array($index)) {
-                    $html .= '
-                    <li role="presentation" class="list-group-item '.$alias.'">
-                        <a class="collapsed" role="button" data-toggle="collapse" href="#coll'.$alias.$id.'" aria-expanded="false" aria-controls="coll'.$alias.$id.'"><span class="caret"></span></a>
-                        <a href="'.$url.'" class="">'.$title.'</a>
-                    <ul id="coll'.$alias.$id.'" class="list-group collapse" aria-labelledby="coll'.$alias.$id.'Heading" aria-expanded="true">';
-                    $html .= frontMenuBuildCategory($index, $res, $active);
-                    $html .= '</ul></li>';
-                } else {
-                    if ($i !== 1) $html .= '</li>';
-                    if (is_array($value['children'])) {
-                    } else {
-                		$title = $res[$index]['title'];
-		                $alias = ($active == $res[$index]['alias'])?' active':'';
-                        $html .= '<li class="list-group-item '.$alias.'">
-                            <a><span class="glyphicon glyphicon-link" aria-hidden="true"></span></a>
-                            <a href="'.$url.'">'.$title.'</a>';
-                    }
-                }
-            }
-        }
-    }
-    $html .= '</li>';
-    return $html;
-}
+
 $currCat = ($res->categoryCurrent->id) ? $res->categoryCurrent->id : null ;
 $CatType = json_decode($res->categoryItems->$modRes['params']['categorytype']->params, true);
 $CatItems = json_decode(json_encode($res->categoryItems->$modRes['params']['categorytype']->items), true);
 echo '
 <div class="panel panel-warning nav-category">
-    <div class="panel-heading">'.$CatItems[$CatType[0]['id']]['title'].'</div>
+    <div class="panel-heading">
+    	<a href="'.menuLinkBuilder('category', $CatType[0]['id']).'">'.$CatItems[$CatType[0]['id']]['title'].'</a>
+    </div>
     <ul class="list-group">
 ';
 echo frontMenuBuildCategory($CatType[0]['children'], $CatItems, $currCat);
--- core/modules/mod-menu/menu.php
+++ core/modules/mod-menu/menu.php
@@ -20,42 +20,13 @@ defined('ISsCMS') or die;
  * @param 	String 	$active Alias menu item that is Active
  * @return 	HTML 			Return builded HTML
  */
-function frontMenuBuild($params, $res, $active) {
-    $html = '';
-    foreach($params as $key => $value) {
-        $i == 0;
-        foreach($value as $key => $index) {
-            $i++;
-            if($key == 'id') $tempId = $index;
-            if ($res[$value['id']]['id'] == $value['id']) {
-                $url = menuLinkBuilder('menu', $res[$value['id']]['id']);
-                $title = $res[$value['id']]['title'];
-                $alias = ($active == $res[$value['id']]['alias'])?' active':'';
-                if(is_array($index)) {
-                    $html .= '
-                    <li class="btn-group'.$alias.'">
-                        <a href="'.$url.'" class="btn">'.$title.'</a>
-                        <a href="#" class="dropdown-toggle btn" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
-                    <ul class="dropdown-menu">';
-                    $html .= frontMenuBuild($index, $res, $active);
-                    $html .= '</ul></li>';
-                } else {
-                    if ($i !== 1) $html .= '</li>';
-                    if (is_array($value['children'])) {
-                    } else {
-                		$title = $res[$index]['title'];
-		                $alias = ($active == $res[$index]['alias'])?' class="active"':'';
-                        $html .= '<li'.$alias.'><a href="'.$url.'">'.$title.'</a>';
-                    }
-                }
-            }
-        }
-    }
-    $html .= '</li>';
-    return $html;
-}
 
-echo frontMenuBuild(json_decode($res->menuItems->$modRes['params']['menutype']->params, true), json_decode(json_encode($res->menuItems->$modRes['params']['menutype']->items), true), $res->menuItemCurrent->alias);
+$tempMenuTypeRes = $db->get("menutype", '*', [
+    "id" => $modRes['params']['menutype']
+]);
+$modRes['params']['_menutype_name'] = $tempMenuTypeRes['name'];
+
+echo frontMenuBuild(json_decode($res->menuItems->$modRes['params']['_menutype_name']->params, true), json_decode(json_encode($res->menuItems->$modRes['params']['_menutype_name']->items), true), $res->menuItemCurrent->alias);
 
 // include $modPathView;
 ?>
